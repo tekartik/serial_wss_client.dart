5a087e4b42346acc8fd291d9460ee967ed352869
Alexandre Roux Tekartik <alex@tekartik.com>
2017-08-22 18:15:09 +0200

    build

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,8 +7,18 @@ dartanalyzer --fatal-warnings \
   lib/message.dart \
   lib/constant.dart \
   lib/serial_wss_client.dart \
-  lib/service/browser.dart \
-  lib/service/io.dart \
+  lib/service/serial_stream_channel_service.dart \
   lib/service/serial_wss_client_service.dart \
+  lib/channel/io.dart \
+  lib/channel/memory.dart \
+  lib/channel/native.dart \
+  lib/channel/web_socket_channel.dart \
+  lib/channel/client/io.dart \
+  lib/channel/client/memory.dart \
+  lib/channel/client/browser.dart \
+  lib/channel/client/web_socket_channel.dart \
+  lib/channel/server/io.dart \
+  lib/channel/server/memory.dart \
+  lib/channel/server/web_socket_channel_server.dart \
 
 pub run test -p vm,firefox,chrome -j 1
